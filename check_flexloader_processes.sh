2db1b7170cb6dc39bc305c11f31922584ec3c80a
Developer <developher@yandex.ru>
2024-12-28 14:57:41 +0300

    Add short process name extraction

--- check_flexloader_processes.sh
+++ check_flexloader_processes.sh
@@ -4,12 +4,28 @@
 FLEXLOADER_HOME="/opt/flexloader"  # Измените на нужный путь
 CURRENT_USER=$(whoami)
 
+# Функция для получения короткого имени процесса
+get_short_name() {
+    local full_name=$1
+    case $full_name in
+        *"extract-all"*) echo "extract-all" ;;
+        *"meta-init"*) echo "meta-init" ;;
+        *"target-init"*) echo "target-init" ;;
+        *"apply-all"*) echo "apply-all" ;;
+        *) echo "$full_name" ;;
+    esac
+}
+
 # Функция для проверки и вывода процесса
 check_process() {
     local process_name=$1
     ps -ef | grep "$process_name" | grep "$CURRENT_USER" | grep "$FLEXLOADER_HOME" | grep -v grep | \
-    awk -v home="$FLEXLOADER_HOME" -v user="$CURRENT_USER" \
-        '{print home "\t" user "\t" $8 "\t" $2}'
+    while read -r line; do
+        pid=$(echo "$line" | awk '{print $2}')
+        cmd=$(echo "$line" | awk '{print $8}')
+        short_name=$(get_short_name "$cmd")
+        echo -e "$FLEXLOADER_HOME\t$CURRENT_USER\t$short_name\t$pid"
+    done
 }
 
 # Список процессов для проверки
