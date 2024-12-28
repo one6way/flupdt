36e309579b950da1094ebb173d09c93bcbbde0dd
Developer <developher@yandex.ru>
2024-12-28 14:53:27 +0300

    Customize output format to show only directory, user, process and PID

--- check_flexloader_processes.sh
+++ check_flexloader_processes.sh
@@ -2,28 +2,26 @@
 
 # Определение переменных окружения
 FLEXLOADER_HOME="/opt/flexloader"  # Измените на нужный путь
+CURRENT_USER=$(whoami)
 
-# Функция для проверки и вывода PID процесса
+# Функция для проверки и вывода процесса
 check_process() {
     local process_name=$1
-    local count=0
-    echo "Процесс: $process_name"
-    ps -f -u $(whoami) | grep "$FLEXLOADER_HOME" | grep "$process_name" | grep -v grep | while read -r line; do
-        count=$((count + 1))
-        pid=$(echo "$line" | awk '{print $2}')
-        path=$(echo "$line" | awk '{print $NF}')
-        echo "  PID $count: $pid\t$path"
-    done
-    echo "-------------------"
+    ps -ef | grep "$process_name" | grep "$CURRENT_USER" | grep "$FLEXLOADER_HOME" | grep -v grep | \
+    awk -v home="$FLEXLOADER_HOME" -v user="$CURRENT_USER" \
+        '{print home "\t" user "\t" $8 "\t" $2}'
 }
 
 # Список процессов для проверки
 PROCESSES_TO_CHECK=(
     "ru.glowbyte.flexloader.CLI extract-all"
-    "ru.glowbyte.flexloader.CLI meta-init"
-    "ru.glowbyte.flexloader.CLI target-init"
+    "ru.glowbyte.flexloader.CLI meta-init-service"
+    "ru.glowbyte.flexloader.CLI target-init-service"
     "ru.glowbyte.flexloader.CLI apply-all"
 )
+
+echo -e "ПАПКА\tПОЛЬЗОВАТЕЛЬ\tПРОЦЕСС\tPID"
+echo "----------------------------------------"
 
 # Проверка каждого процесса
 for process in "${PROCESSES_TO_CHECK[@]}"; do
