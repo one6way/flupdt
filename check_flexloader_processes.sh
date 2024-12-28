d9a2fe172fd987d21e9d394a379268e39c160a6e
Developer <developher@yandex.ru>
2024-12-28 15:04:08 +0300

    Rewrite process detection with direct field reading

--- check_flexloader_processes.sh
+++ check_flexloader_processes.sh
@@ -4,31 +4,33 @@
 FLEXLOADER_HOME="/opt/flexloader"  # Измените на нужный путь
 CURRENT_USER=$(whoami)
 
-# Функция для получения короткого имени процесса
-get_short_name() {
-    local full_name=$1
-    if [[ $full_name == *"extract-all"* ]]; then
-        echo "extract-all"
-    elif [[ $full_name == *"meta-init"* ]]; then
-        echo "meta-init"
-    elif [[ $full_name == *"target-init"* ]]; then
-        echo "target-init"
-    elif [[ $full_name == *"apply-all"* ]]; then
-        echo "apply-all"
-    else
-        echo "$full_name"
-    fi
-}
-
 # Функция для проверки и вывода процесса
 check_process() {
-    local process_name=$1
-    local short_name=$(get_short_name "$process_name")
-    ps -ef | grep "$process_name" | grep "$CURRENT_USER" | grep "$FLEXLOADER_HOME" | grep -v grep | \
-    while read -r line; do
-        pid=$(echo "$line" | awk '{print $2}')
-        echo -e "$FLEXLOADER_HOME\t$CURRENT_USER\t$short_name\t$pid"
-    done
+    local search_pattern=$1
+    local service_type
+    
+    # Определяем тип сервиса из шаблона поиска
+    if [[ $search_pattern == *"extract-all"* ]]; then
+        service_type="extract-all"
+    elif [[ $search_pattern == *"meta-init"* ]]; then
+        service_type="meta-init"
+    elif [[ $search_pattern == *"target-init"* ]]; then
+        service_type="target-init"
+    elif [[ $search_pattern == *"apply-all"* ]]; then
+        service_type="apply-all"
+    else
+        service_type="unknown"
+    fi
+    
+    # Используем wc -l для подсчета строк и избегания пустого вывода
+    local count=$(ps -ef | grep "$search_pattern" | grep "$CURRENT_USER" | grep "$FLEXLOADER_HOME" | grep -v grep | wc -l)
+    
+    if [ "$count" -gt 0 ]; then
+        ps -ef | grep "$search_pattern" | grep "$CURRENT_USER" | grep "$FLEXLOADER_HOME" | grep -v grep | \
+        while read -r user pid ppid rest; do
+            echo -e "$FLEXLOADER_HOME\t$CURRENT_USER\t$service_type\t$pid"
+        done
+    fi
 }
 
 # Список процессов для проверки
